9cf16fa045d3083fa306da400a774e5828ff4066
Edanur Özkaplan <edanurozkaplan05@gmail.com>
2026-06-03 22:57:16 +0300

    Sprint 2 - Improved CNN with BatchNorm and Dropout

---
+++ evaluate_improved.py
@@ -0,0 +1,12 @@
+import numpy as np
+import tensorflow as tf
+
+model = tf.keras.models.load_model("improved_model.keras")
+
+X_test = np.load("X_test.npy")
+y_test = np.load("y_test.npy")
+
+loss, accuracy = model.evaluate(X_test, y_test)
+
+print("Test Accuracy:", accuracy)
+print("Test Loss:", loss)
